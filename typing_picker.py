9c9a582d15df43dc1346f39771342eee1ac5a94b
lucywheeler <lucykate100@outlook.com>
2016-02-26 17:52:16 -0800

    Create typing_picker.py

---
+++ typing_picker.py
@@ -0,0 +1,10 @@
+# import the magic random picker
+
+# list of things to pick from
+typing_options = ["Ben", "Lucy", "Jude", "Max"]
+
+# find a way to pick something
+typing_choice = typing_options[0]
+
+# a way to tell the user what to do
+print typing_choice
